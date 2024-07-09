8ac283156a972429ec78402f0db1968669e2ec71
0lgaP1 <volhapatapchuk@gmail.com>
2024-07-09 22:30:52 +0200

    AddItemForm component

---
+++ 1-todolist/src/AddItemForm.tsx
@@ -0,0 +1,48 @@
+import React, {ChangeEvent, KeyboardEvent, useState} from "react";
+import {Button} from "./Button";
+
+export type AddTodoListPropsType = {
+    addTask: (title: string, todolistId: string) => void;
+    id: string;
+}
+
+export function AddItemForm(props: AddTodoListPropsType) {
+    const [taskTitle, setTaskTitle] = useState(""); //отслеживаем ввод пользователя
+    const [error, setError] = useState<string | null>(null);
+    const isAddBtnDisabled = taskTitle.length === 0 || taskTitle.length > 15
+    const changeTaskTitleHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setTaskTitle(e.currentTarget.value);
+    };
+    const addTaskOnKeyUpHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === "Enter" && taskTitle) {
+            addTaskHandler()
+        }
+    }
+    const addTaskHandler = () => {
+        const trimmedTaskTitle = taskTitle.trim()
+        if (trimmedTaskTitle !== "") {
+            props.addTask(props.id, taskTitle);
+            setError(null);
+        } else {
+            setError("Title is required")
+        }
+        setTaskTitle("");
+    }
+
+    return <div>
+        <input className={error ? "task-input-error" : ""}
+               value={taskTitle}
+               onChange={changeTaskTitleHandler}
+               onKeyUp={addTaskOnKeyUpHandler}/>
+        <Button title="+"
+                onClickHandler={addTaskHandler}
+                disabled={isAddBtnDisabled}/>
+
+        {taskTitle.length > 10 && taskTitle.length <= 15 ? (
+            <div>Use not more than 10 characters</div>
+        ) : taskTitle.length > 15 ? (
+            <div>You used more than 15 characters</div>
+        ) : null}
+        {error && <div>{error}</div>}
+    </div>
+}
